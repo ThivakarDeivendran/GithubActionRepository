b94aec3318259003be104f48c41c1dbd3d46342d
ThivakarDeivendran <dthivakar1@gmail.com>
2023-04-08 20:44:44 +0530

    first update Github actions

---
+++ src/test/java/QATestingPackage/QATestingClass.java
@@ -0,0 +1,13 @@
+package QATestingPackage;
+
+public class QATestingClass {
+	
+	public static void main(String[] args) {
+		
+		int a =66;
+		int b = 77;
+		int add = a+b ;
+		System.out.println(add);	
+	}
+	
+}
